b52f8f899e97809fab63c20629e17f98f863b38c
Yura <gyrra777@gmail.com>
2023-03-01 23:02:04 +0300

    25-ое задание добавил случайное число

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,13 @@
+import random
+
 from model.project import Project
 
 
 def test_add_project(app):
     app.session.login("administrator", "root")
     old_projects = app.project.get_projects_list()
-    app.project.create(Project(name="Test2", status="release", viewStatus="public",
-                 description="test",inherit_global=True))
-    new_projects = app.project.get_projects_list() 
+    c = random.randrange(10)
+    app.project.create(Project(name="Test_name_" + c, status="release", viewStatus="public",
+                               description="test", inherit_global=True))
+    new_projects = app.project.get_projects_list()
     assert len(old_projects) + 1 == len(new_projects)
