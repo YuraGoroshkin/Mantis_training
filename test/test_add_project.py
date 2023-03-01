e3c564d23cdfc570abc646e146bb929a08cc7166
Yura <gyrra777@gmail.com>
2023-03-01 23:03:09 +0300

    25-ое задание добавил случайное число - str

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -6,7 +6,7 @@ from model.project import Project
 def test_add_project(app):
     app.session.login("administrator", "root")
     old_projects = app.project.get_projects_list()
-    c = random.randrange(10)
+    c = str(random.randrange(10))
     app.project.create(Project(name="Test_name_" + c, status="release", viewStatus="public",
                                description="test", inherit_global=True))
     new_projects = app.project.get_projects_list()
